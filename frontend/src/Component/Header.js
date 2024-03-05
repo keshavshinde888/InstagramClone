6d4c1faf846ef382065ad52394117f31da0a4c88
keshavshinde888 <147683001+keshavshinde888@users.noreply.github.com>
2024-03-05 16:23:25 +0530

    code commit

--- frontend/src/Component/Header.js
+++ frontend/src/Component/Header.js
@@ -1,8 +1,21 @@
-import React from 'react'
-export const Header=()=>{
-      return(
-        <div className='header'>
-            Instagram
-        </div>
-      )
-}
+import React from "react";
+export const Header = () => {
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    // Do something with the selected file, such as uploading it to a server
+    console.log("Selected File:", selectedFile);
+  };
+  return (
+    <div className="header">
+      <div className="file-upload-container">
+        <input
+          type="file"
+          id="file-input"
+          onChange={handleFileChange}
+          className="file-input"
+        />
+      </div>
+      <div>Instagram</div>
+    </div>
+  );
+};
